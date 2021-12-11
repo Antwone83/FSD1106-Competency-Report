92aab822f446fbec5774a950aec6ebd16511b0df
Antwone83 <92768235+Antwone83@users.noreply.github.com>
2021-12-11 11:54:55 -0800

    Add files via upload

---
+++ script/task.js
@@ -0,0 +1,13 @@
+class Task {
+    constructor(priority, title, desc, dueDate, status, category, color, Antwone) {
+        this.priority = priority;
+        this.title = title;
+        this.description = desc;
+        this.dueDate = dueDate;
+        this.status = status;
+        this.category = category;
+        this.color = color;
+
+        this.name = "Antwone";
+    }
+}
